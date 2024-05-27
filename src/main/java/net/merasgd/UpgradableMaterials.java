031ffe8c226d5dde3f2636dc4f0b5ce7a8bde294
MerasGD <discordmeras69@gmail.com>
2024-05-27 18:40:10 +0800

    first commit

---
+++ src/main/java/net/merasgd/UpgradableMaterials.java
@@ -0,0 +1,18 @@
+package net.merasgd;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UpgradableMaterials implements ModInitializer {
+	public static final String MOD_ID = "";
+    public static final Logger LOGGER = LoggerFactory.getLogger("upgradable");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.debug("");
+
+	}
+}
---
+++ src/main/java/net/merasgd/UpgradableMaterialsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.merasgd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class UpgradableMaterialsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
